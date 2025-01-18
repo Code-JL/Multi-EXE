70090190bb58a81349cb80f21c6b1927e73b6637
Code-JL <189483298+Code-JL@users.noreply.github.com>
2025-01-18 17:07:45 -0400

    Feat: Uses windows.h ShellExecute to open 2 files.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <windows.h> // not cross platform. Switch to boost or QT for cross platform
 
+
+void execute(std::string file) {
+	std::wstring wfile(file.begin(), file.end());
+
+	ShellExecute(NULL, TEXT("open"), wfile.c_str(), NULL, NULL, SW_SHOWDEFAULT);
+}
 int main() {
-	std::string file1 = "";
-	std::string file2 = "";
+	std::string file1 = "C:\Programs\Fab\Fab_x64.exe";
+	std::string file2 = "https://app.tuta.com/mail/O7v3CT--0o-9";
 
-	std::cout << file1 + file2 << std::endl;
+	execute(file1);
+
+	execute(file2);
 }
