5b4db9905266776d8a97702188b3cc324695c129
Code-JL <189483298+Code-JL@users.noreply.github.com>
2025-01-20 06:20:09 -0400

    Feat: initial menu added, fileName() for getting file name.

--- main.cpp
+++ main.cpp
@@ -7,11 +7,57 @@ void execute(std::string file) {
 
 	ShellExecute(NULL, TEXT("open"), wfile.c_str(), NULL, NULL, SW_SHOWDEFAULT);
 }
+
+
+std::string fileName(std::string filePath) {
+
+	int startPos = filePath.find_last_of("\\");
+
+	return (filePath.substr(startPos));
+}
+/*
+void filesView(bool done = false) {
+
+	std::cout << 
+	
+	filesView()
+}
+*/
 int main() {
-	std::string file1 = "C:\Programs\Fab\Fab_x64.exe";
+
+	int ans;
+
+	std::cout << "What would you like to do?" << std::endl;
+	std::cout << "execute files (1)" << std::endl;
+	std::cout << "View current files (2)" << std::endl;
+	std::cout << "Exit (0)" << std::endl;
+
+	std::cin >> ans;
+
+
+	switch (ans) {
+	case 0:
+		exit;
+	case 1:
+		//execute
+		break;
+
+	case 2:
+		//viewing menu for all current files
+		break;
+
+	default:
+		std::cout << "incorrect input." << std::endl;
+	}
+
+
+
+	std::string file1 = R"(C:\Programs\Fab\Fab_x64.exe)";
+
+	std::cout << "\\" << std::endl;
 	std::string file2 = "https://app.tuta.com/mail/O7v3CT--0o-9";
 
-	execute(file1);
+	//execute(file1);
 
-	execute(file2);
+	//execute(file2);
 }
